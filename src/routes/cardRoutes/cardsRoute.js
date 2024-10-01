df2fa1435292f61f452ccb849d948013930f82e2
rolandeke <roladneke49@gmail.com>
2024-10-01 13:33:38 +0000

    updated code for cardRoute

--- src/routes/cardRoutes/cardsRoute.js
+++ src/routes/cardRoutes/cardsRoute.js
@@ -3,29 +3,17 @@ const validator = require("../../utils/Validators");
 const card = require("../../models/Card");
 const route = express.Router();
 const jwt = require("jsonwebtoken");
+const { default: mongoose } = require("mongoose");
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //add card
 
 // TODO -> CREATE A SPERATE CONTROLLER FILE
 route.post("/addcard", async (req, res) => {
-  // TODO MOVE TO AUTH MIDDLEWARE FUNCTION
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res
-      .status(401)
-      .json({ error: "Authorization header missing or incorrect format." });
-  }
-
-  const token = authHeader.split(" ")[1]; // Extract the token from "Bearer token"})
-
-  const { cardNumber, expiryDate, cvv, cardHolderName, cardType, zipCode } =
-    req.body;
   try {
-    // TODO MOVE TO AUTH MIDDLEWARE FUNCTION
-    const decoded = jwt.verify(token, "your-secret-key");
+    const { cardNumber, expiryDate, cvv, cardHolderName, cardType, zipCode } =
+      req.body;
+    const { userId } = req.user;
 
-    // TODO MOVE TO A SEPERATE VALIDATOR FILE AND IMPORT
     const valid = validator.cardValidator(
       cardNumber,
       expiryDate,
@@ -38,38 +26,36 @@ route.post("/addcard", async (req, res) => {
       return res
         .status(400)
         .json({ statusCode: 400, error: valid.error.details[0].message });
-    } else {
-      // TODO - HANDLE BUSINESS LOGIC IN A SERVICE FILE
-      const existingCard = await card.findOne({ cardNumber: cardNumber });
-      if (existingCard) {
-        return res.status(400).json({
-          statusCode: 400,
-          error: "Card already exists",
-        });
-      }
-
-      // TODO use async/await
-      card
-        .create({
-          userId: decoded.id,
-          cardNumber: cardNumber,
-          expiryDate: expiryDate,
-          cvv: cvv,
-          cardHolderName: cardHolderName,
-          cardType: cardType,
-          zipCode: zipCode,
-        })
-        .then((response) => {
-          return res.status(201).json({
-            message: `card sucessfully Added`,
-            user: response,
-            statusCode: 201,
-          });
-        })
-        .catch((error) => {
-          res.status(400).json({ message: error.message, statusCode: 400 });
-        });
     }
+
+    const existingCard = await card.findOne({ cardNumber: cardNumber });
+    if (existingCard) {
+      return res.status(400).json({
+        statusCode: 400,
+        error: "Card already exists",
+      });
+    }
+
+    card
+      .create({
+        userId,
+        cardNumber: cardNumber,
+        expiryDate: expiryDate,
+        cvv: cvv,
+        cardHolderName: cardHolderName,
+        cardType: cardType,
+        zipCode: zipCode,
+      })
+      .then((response) => {
+        return res.status(201).json({
+          message: `card sucessfully Added`,
+          user: response,
+          statusCode: 201,
+        });
+      })
+      .catch((error) => {
+        res.status(400).json({ message: error.message, statusCode: 400 });
+      });
   } catch (error) {
     res.status(500).json({
       message: error.message,
@@ -79,21 +65,12 @@ route.post("/addcard", async (req, res) => {
 });
 //update card
 route.put("/update/:id", async (req, res) => {
-  const id = req.params.id;
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res
-      .status(401)
-      .json({ error: "Authorization header missing or incorrect format." });
-  }
-
-  const token = authHeader.split(" ")[1];
-  const { cardNumber, expiryDate, cvv, cardHolderName, cardType, zipCode } =
-    req.body;
-
   try {
-    const decoded = jwt.verify(token, "your-secret-key");
+    const id = req.params.id;
+    const { userId } = req.user;
 
+    const { cardNumber, expiryDate, cvv, cardHolderName, cardType, zipCode } =
+      req.body;
     const valid = validator.cardValidator(
       cardNumber,
       expiryDate,
@@ -106,55 +83,43 @@ route.put("/update/:id", async (req, res) => {
       return res
         .status(400)
         .json({ statusCode: 400, error: valid.error.details[0].message });
-    } else {
-      const update = await card.findOneAndUpdate(
-        { _id: id, userId: decoded.id },
-        {
-          cardNumber: cardNumber,
-          expiryDate: expiryDate,
-          cvv: cvv,
-          cardHolderName: cardHolderName,
-          cardType: cardType,
-          zipCode: zipCode,
-        },
-        { returnOriginal: false } // Option: return the updated document
-      );
-      // If no card was found, return 404
-      if (!update) {
-        return res.status(404).json({
-          message: "Card not found",
-          statusCode: 404,
-        });
-      }
-
-      // Return the updated card
-      return res.status(200).json({
-        message: "Card updated successfully",
-        card: update,
-        statusCode: 200,
+    }
+    const update = await card.findOneAndUpdate(
+      { _id: id, userId },
+      {
+        cardNumber: cardNumber,
+        expiryDate: expiryDate,
+        cvv: cvv,
+        cardHolderName: cardHolderName,
+        cardType: cardType,
+        zipCode: zipCode,
+      },
+      { returnOriginal: false } // Option: return the updated document
+    );
+    // If no card was found, return 404
+    if (!update) {
+      return res.status(404).json({
+        message: "Card not found",
+        statusCode: 404,
       });
     }
+
+    // Return the updated card
+    return res.status(200).json({
+      message: "Card updated successfully",
+      card: update,
+      statusCode: 200,
+    });
   } catch (error) {
     return res.status(500).json({ statusbar: 500, message: error.message });
   }
 });
 //delete card
 route.delete("/delete/:id", async (req, res) => {
-  const id = req.params.id;
-
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res
-      .status(401)
-      .json({ error: "Authorization header missing or incorrect format." });
-  }
-
-  const token = authHeader.split(" ")[1]; // Extract the token from "Bearer token"})
-
   try {
-    const decoded = jwt.verify(token, "your-secret-key");
-    const Card = await card.findOneAndDelete({ _id: id, userId: decoded.id });
+    const { userId } = req.user;
+    const id = req.params.id;
+    const Card = await card.findOneAndDelete({ _id: id, userId });
     if (!Card) {
       return res.status(404).json({
         message: "Card not found",
@@ -177,11 +142,7 @@ route.get("/all", async (req, res) => {
     const { userId } = req.user;
 
     const allCards = await card.find({ userId });
-    // if (!allCards) {
-    //   return res.status(404).json({
-    //     message: "no cards available",
-    //   });
-    // }
+
     res.status(200).json({
       message: "All Cards",
       Cards: allCards,
@@ -191,21 +152,17 @@ route.get("/all", async (req, res) => {
   }
 });
 //get card by id
-route.get("/:id", async (req, res) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res
-      .status(401)
-      .json({ error: "Authorization header missing or incorrect format." });
-  }
-
-  const token = authHeader.split(" ")[1]; // Extract the token from "Bearer token"})
+route.get("/:id", async (req, res, next) => {
   try {
-    const decoded = jwt.verify(token, "your-secret-key");
-
+    const { userId } = req.user;
     const id = req.params.id;
-    const singleCard = await card.findOne({ _id: id, userId: decoded.id });
+    if (!mongoose.isValidObjectId(id)) {
+      return res.status(400).json({
+        message: "Invalid Card Id",
+        statusCode: 400,
+      });
+    }
+    const singleCard = await card.findOne({ _id: id, userId });
 
     if (!singleCard) {
       return res.status(404).json({
@@ -219,7 +176,9 @@ route.get("/:id", async (req, res) => {
       card: singleCard,
       statusCode: 200,
     });
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = route;
